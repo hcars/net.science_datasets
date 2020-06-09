558066681eaaba102ea5a22817fa67b97915c1dc
hcars <hcarscad@gmail.com>
2020-06-09 10:46:08 -0400

    changed database scripts

--- download_scripts/validate_in.py
+++ download_scripts/validate_in.py
@@ -5,6 +5,6 @@ conn = db.connect('../graph_metadata.db')
 cursor = conn.cursor()
 
 for edge_list in glob('../*networks/edge_lists/*'):
-         print(cursor.execute('SELECT name FROM graphs_downloaded WHERE edgelist_path=?', (edge_list,))
-
-
+         cursor.execute('SELECT name FROM graphs_downloaded WHERE edgelist_path=?', (edge_list,))
+         print(edge_list)
+         print(cursor.fetchone())
