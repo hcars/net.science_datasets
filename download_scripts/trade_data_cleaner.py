a53cfe77c3d63ffd8fc15755030f9039325f6f88
hcars <hcarscad@gmail.com>
2020-06-16 10:39:29 -0400

    Trade conversionn script

---
+++ download_scripts/trade_data_cleaner.py
@@ -0,0 +1,11 @@
+import csv
+base_file = '../trade_networks/year_origin_destination_sitc_rev2.tsv'
+
+base_fp = open(base_file, 'r')
+
+for line in base_fp:
+    line_split = line.strip('\n').split('\t')
+    if line_split[0].isnumeric():
+        with open('trade-' + line_split[0] + '.csv', 'a', newline='') as individual_entry:
+            csv_writer = csv.writer(individual_entry)
+            csv_writer.writerow(line_split)
